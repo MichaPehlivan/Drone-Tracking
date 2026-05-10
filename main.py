cd422ab4fec811793e3da9b13ee8314fdc5860d3
MichaPehlivan <michapehlivan2004@gmail.com>
2026-05-11 01:11:17 +0200

    cleaned up main

--- main.py
+++ main.py
@@ -16,77 +16,6 @@ from Tracking_Routines import (
 )
 from Utils import animate_track
 
-# # NORMAL KALMAN FILTER
-# # Initialize values
-# dt = 0.1
-# x_initial = 0
-# y_initial = 0
-# measurement_sigma = 0  # standard deviation of the measurement
-# var = measurement_sigma**2
-
-# num_datapoints = 10
-# # Initialize the simulated measurements.
-# measurements, trueTrack = simulateLinearTrack(
-#     v_x=10,
-#     v_y=10,
-#     x0=x_initial,
-#     y0=y_initial,
-#     num_datapoints=num_datapoints,
-#     dt=dt,
-#     sigma=measurement_sigma,
-# )
-
-# # Initialize the matrices for the Kalman filter.
-# F = np.array([[1, 0, dt, 0], [0, 1, 0, dt], [0, 0, 1, 0], [0, 0, 0, 1]])
-
-# H = np.array([[1, 0, 0, 0], [0, 1, 0, 0]])
-
-# # TODO: Find appropriate covariance matrices.
-# Q = 0.0001 * np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
-
-# R = 1 * np.array([[var, 0], [0, var]])
-
-# x0 = np.array([[x_initial], [y_initial], [1], [1]])
-
-# P0 = 1000 * np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
-
-# # Run the kalman filter.
-# RunSimpleKalman(F, H, Q, R, x0, P0, measurements, trueTrack)
-
-
-# # Run another with high sigma (high ospa distance)
-# measurement_sigma = 0.5
-# var = measurement_sigma**2
-# R = np.array([[var, 0], [0, var]])
-# measurements, trueTrack = simulateLinearTrack(
-#     v_x=10,
-#     v_y=10,
-#     x0=x_initial,
-#     y0=y_initial,
-#     num_datapoints=num_datapoints,
-#     dt=dt,
-#     sigma=measurement_sigma,
-# )
-
-# RunSimpleKalman(F, H, Q, R, x0, P0, measurements, trueTrack)
-
-
-# # Another with zero sigma (zero ospa distance)
-# measurement_sigma = 5
-# var = measurement_sigma**2
-# R = np.array([[var, 0], [0, var]])
-# measurements, trueTrack = simulateLinearTrack(
-#     v_x=10,
-#     v_y=10,
-#     x0=x_initial,
-#     y0=y_initial,
-#     num_datapoints=num_datapoints,
-#     dt=dt,
-#     sigma=measurement_sigma,
-# )
-
-# RunSimpleKalman(F, H, Q, R, x0, P0, measurements, trueTrack)
-
 # EXTENDED KALMAN FILTER
 # Initialize values
 dt = 0.4
@@ -96,28 +25,6 @@ measurement_sigma = 0  # standard deviation of the measurement
 var = measurement_sigma**2
 
 num_datapoints = 60
-
-# Initialize the simulated measurements.
-measurements_cartesian, trueTrack = simulateLinearTrack(
-    v_x=10,
-    v_y=10,
-    x0=x_initial,
-    y0=y_initial,
-    num_datapoints=num_datapoints,
-    dt=dt,
-    sigma=measurement_sigma,
-)
-
-measurements_polar, trueTrack = simulateRandomAccelTrackPolar(
-    v_x=10,
-    v_y=10,
-    x0=x_initial,
-    y0=y_initial,
-    num_datapoints=num_datapoints,
-    dt=dt,
-    sigma_r=measurement_sigma,
-    sigma_phi=measurement_sigma,
-)
 
 # Initialize the functions and matrices for the Kalman filter.
 f = lambda x: np.dot(
@@ -148,60 +55,21 @@ H_polar = lambda x: np.array(
 )
 
 # TODO: Find appropriate covariance matrices.
-Q = 1.0 * np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+Q = 1 * np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
 
-R = 100 * np.array([[var, 0], [0, var]])
+R = 1 * np.array([[var, 0], [0, var]])
 
 x0 = np.array([[x_initial], [y_initial], [1], [1]])
 
 P0 = 1 * np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
 
-# Run the kalman filter with no noise.
-# RunExtendedKalman(
-#     f,
-#     h_cartesian,
-#     F,
-#     H_cartesian,
-#     Q,
-#     R,
-#     x0,
-#     P0,
-#     measurements_cartesian,
-#     trueTrack,
-#     polar=False,
-# )  # cartesian
-
-# RunExtendedKalman(
-#     f, h_polar, F, H_polar, Q, R, x0, P0, measurements_polar, trueTrack
-# )  # polar
-#
-# # Run another with low sigma
-# range_sigma = 0.5
-# azimuth_sigma = 0.02  # 1.14 degrees
-# var_r = range_sigma**2
-# var_phi = azimuth_sigma**2
-# R = np.array([[var_r, 0], [0, var_phi]])
-# measurements, trueTrack = simulateLinearTrackPolar(
-#     v_x=10,
-#     v_y=10,
-#     x0=x_initial,
-#     y0=y_initial,
-#     num_datapoints=num_datapoints,
-#     dt=dt,
-#     sigma_r=range_sigma,
-#     sigma_phi=azimuth_sigma,
-# )
-
-# RunExtendedKalman(f, h_polar, F, H_polar, Q, R, x0, P0, measurements_polar, trueTrack)
-# np.random.seed(3000)
-
-# Another with high sigma
-range_sigma = 1
-azimuth_sigma = np.deg2rad(5)
+# Run with no noise
+range_sigma = 0
+azimuth_sigma = np.deg2rad(0)
 var_r = range_sigma**2
 var_phi = azimuth_sigma**2
 R = np.array([[var_r, 0], [0, var_phi]])
-measurements, trueTrack = simulateRandomAccelTrackPolar(
+measurements, trueTrack = simulateRandomAccelHoverTrackPolar(
     v_x=1,
     v_y=1,
     x0=x_initial,
@@ -212,5 +80,45 @@ measurements, trueTrack = simulateRandomAccelTrackPolar(
     sigma_phi=azimuth_sigma,
 )
 
-animate_track(trueTrack, dt=dt)
-RunJointKalman(f, h_polar, F, H_polar, Q, R, x0, P0, 0.1, 2, 0, measurements, trueTrack)
+# animate_track(trueTrack, dt=dt)
+RunJointKalman(f, h_polar, F, H_polar, Q, R, x0, P0, 0.5, 2, 0, measurements, trueTrack)
+
+# Another with low sigma
+range_sigma = 1
+azimuth_sigma = np.deg2rad(3)
+var_r = range_sigma**2
+var_phi = azimuth_sigma**2
+R = np.array([[var_r, 0], [0, var_phi]])
+measurements, trueTrack = simulateRandomAccelHoverTrackPolar(
+    v_x=1,
+    v_y=1,
+    x0=x_initial,
+    y0=y_initial,
+    num_datapoints=num_datapoints,
+    dt=dt,
+    sigma_r=range_sigma,
+    sigma_phi=azimuth_sigma,
+)
+
+# animate_track(trueTrack, dt=dt)
+RunJointKalman(f, h_polar, F, H_polar, Q, R, x0, P0, 0.5, 2, 0, measurements, trueTrack)
+
+# Another with high sigma
+range_sigma = 10
+azimuth_sigma = np.deg2rad(5)
+var_r = range_sigma**2
+var_phi = azimuth_sigma**2
+R = np.array([[var_r, 0], [0, var_phi]])
+measurements, trueTrack = simulateRandomAccelHoverTrackPolar(
+    v_x=1,
+    v_y=1,
+    x0=x_initial,
+    y0=y_initial,
+    num_datapoints=num_datapoints,
+    dt=dt,
+    sigma_r=range_sigma,
+    sigma_phi=azimuth_sigma,
+)
+
+# animate_track(trueTrack, dt=dt)
+RunJointKalman(f, h_polar, F, H_polar, Q, R, x0, P0, 0.5, 2, 0, measurements, trueTrack)
