dd56fc6882ee2148f84a7def2cc208ebe2d29f98
MichaPehlivan <michapehlivan2004@gmail.com>
2026-06-01 01:30:53 +0200

    refactored main

--- main.py
+++ main.py
@@ -1,269 +1,281 @@
 # External Imports
 import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
 
 # Internal Packages
+
+from Evaluation_Metrics import ospa_stonesoup
+from Kalman_Filters import UnscentedKalmanFilter, ExtendedKalmanFilter
 from Track_Simulation import (
     simulateLinearTrack,
     simulateLinearTrackPolar,
     simulateRandomAccelTrackPolar,
     simulateRandomAccelHoverTrackPolar,
 )
-from Tracking_Routines import (
-    RunSimpleKalman,
-    RunConvertedKalman,
-    RunExtendedKalman,
-    RunUnscentedKalman,
-    RunJointKalman,
+from Utils.Wrapper_Functions import (
+    SimulatorPolar_stonesoup,
+    SimulatorPolarMultitarget_stonesoup,
+    EKFUpdater,
+    EKFPredictor,
+    UKFPredictor,
+    UKFUpdater,
+    # CustomDistanceMeasure
 )
-from Utils import animate_track
-from Utils.KalmanBenchmarker import BenchmarkEKF, BenchmarkJoint, BenchmarkUKF
-from Utils.KalmanTuner import (
-    TuneEKF,
-    TuneUKF,
-    optimize_UCMKF,
-    optimize_EKF,
-    optimize_UKF,
-)
+from Utils import ReadDetections, ReadAndClusterDetections
 
-# EXTENDED KALMAN FILTER
+# Stonesoup imports
+from stonesoup.models.measurement.nonlinear import CartesianToBearingRange
+from stonesoup.types.state import GaussianState
+from stonesoup.types.hypothesis import SingleHypothesis
+from stonesoup.types.track import Track
+from stonesoup.dataassociator.neighbour import GlobalNearestNeighbour
+from stonesoup.dataassociator.probability import JPDA
+from stonesoup.deleter.time import UpdateTimeStepsDeleter
+from stonesoup.deleter.error import CovarianceBasedDeleter
+from stonesoup.initiator.simple import MultiMeasurementInitiator
+from stonesoup.measures import Mahalanobis
+from stonesoup.hypothesiser.distance import DistanceHypothesiser
+from stonesoup.plotter import Plotter
+
 # Initialize values
-dt = 0.4
-x_initial = 5
-y_initial = 5
-measurement_sigma = 0  # standard deviation of the measurement
-var = measurement_sigma**2
+dt = 0.0112 * 25
+# dt = 0.1
 
-num_datapoints = 60
 
 # Initialize the functions and matrices for the Kalman filter.
-f_matrix = np.array(
-    [
-        [1, 0, dt, 0, 0.5 * dt**2, 0],
-        [0, 1, 0, dt, 0, 0.5 * dt**2],
-        [0, 0, 1, 0, dt, 0],
-        [0, 0, 0, 1, 0, dt],
-        [0, 0, 0, 0, 1, 0],
-        [0, 0, 0, 0, 0, 1],
-    ]
-)
-f = lambda x: np.dot(
-    f_matrix,
-    x,
-)
-F = lambda x: np.array(
-    [
-        [1, 0, dt, 0, 0.5 * dt**2, 0],
-        [0, 1, 0, dt, 0, 0.5 * dt**2],
-        [0, 0, 1, 0, dt, 0],
-        [0, 0, 0, 1, 0, dt],
-        [0, 0, 0, 0, 1, 0],
-        [0, 0, 0, 0, 0, 1],
-    ]
-)
+# constant velcity (CV) model
+# F_generator = lambda dt: np.array(
+#     [
+#         [1, dt, 0, 0],  # x
+#         [0, 1, 0, 0],  # vx
+#         [0, 0, 1, dt],  # y
+#         [0, 0, 0, 1],  # vy
+#     ]
+# )
+# F = lambda x: F_generator(dt)
 
-h_cartesian = lambda x: np.array([x[0], x[1]])
-h_cartesian_matrix = np.array([[1, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0]])
-H_cartesian = lambda x: h_cartesian_matrix
-h_polar = lambda x: np.array(
-    [[np.arctan2(x[1], x[0])], [np.sqrt(x[0] ** 2 + x[1] ** 2)]]
-)  # conversion to polar
-H_polar = lambda x: np.array(
+# f = lambda x: np.dot(F_generator(dt), x)
+
+# h = lambda x: np.array([np.arctan2(x[2], x[0]), np.sqrt(x[0] ** 2 + x[2] ** 2)])
+
+# H = lambda x: np.array(
+#     [
+#         [
+#             (-x[2]) / (x[0] ** 2 + x[2] ** 2),
+#             0,
+#             x[0] / (x[0] ** 2 + x[2] ** 2),
+#             0,
+#         ],
+#         [
+#             x[0] / np.sqrt(x[0] ** 2 + x[2] ** 2),
+#             0,
+#             x[2] / np.sqrt(x[0] ** 2 + x[2] ** 2),
+#             0,
+#         ],
+#     ]
+# )
+
+# # Process noise matrix.
+# # IMPORTANT: the process noise is dependent on dt
+# # Q = 0.01 * np.eye(6)
+# var_a = 0.5
+# Q1D_generator = lambda dt: var_a * np.array(
+#     [
+#         [(dt**3 / 3), (dt**2 / 2)],
+#         [(dt**2 / 2), dt],
+#     ]
+# )
+# Q_generator = lambda dt: np.block(
+#     [[Q1D_generator(dt), np.zeros((2, 2))], [np.zeros((2, 2)), Q1D_generator(dt)]]
+# )
+
+# Q = Q_generator(dt)
+
+# x0 = np.array([[1], [0], [1], [0]])
+
+# # Starting error covariance (should be on the higher side to quickly settle in towards the correct values. i.e high uncertainty to start with :))
+# P0 = 1 * np.eye(4)
+
+# constant accelaration (CA) model
+F_generator = lambda dt: np.array(
     [
+        [1, dt, 0.5 * dt**2, 0, 0, 0],  # x
+        [0, 1, dt, 0, 0, 0],  # vx
+        [0, 0, 1, 0, 0, 0],  # ax
+        [0, 0, 0, 1, dt, 0.5 * dt**2],  # y
+        [0, 0, 0, 0, 1, dt],  # vy
+        [0, 0, 0, 0, 0, 1],  # ay
+    ]
+)
+F = lambda x: F_generator(dt)
+
+f = lambda x: np.dot(F_generator(dt), x)
+
+h = lambda x: np.array([np.arctan2(x[3], x[0]), np.sqrt(x[0] ** 2 + x[3] ** 2)])
+
+H = lambda x: np.array(
+    [
+        [(-x[3]) / (x[0] ** 2 + x[3] ** 2), 0, 0, x[0] / (x[0] ** 2 + x[3] ** 2), 0, 0],
         [
-            (-1 * x[1]) / (x[0] ** 2 + x[1] ** 2),
-            x[0] / (x[0] ** 2 + x[1] ** 2),
+            x[0] / np.sqrt(x[0] ** 2 + x[3] ** 2),
             0,
             0,
-            0,
-            0,
-        ],  # derivative of arctan2
-        [
-            x[0] / np.sqrt(x[0] ** 2 + x[1] ** 2),
-            x[1] / np.sqrt(x[0] ** 2 + x[1] ** 2),
-            0,
-            0,
+            x[3] / np.sqrt(x[0] ** 2 + x[3] ** 2),
             0,
             0,
         ],
     ]
 )
 
-# TODO: Find appropriate covariance matrices.
-Q = 1.0 * np.array(
+var_a = 0.5
+Q1D_generator = lambda dt: var_a * np.array(
     [
-        [(dt**5) / 20, 0, (dt**4) / 8, 0, (dt**3) / 6, 0],
-        [0, (dt**5) / 20, 0, (dt**4) / 8, 0, (dt**3) / 6],
-        [(dt**4) / 8, 0, (dt**3) / 3, 0, (dt**2) / 2, 0],
-        [0, (dt**4) / 8, 0, (dt**3) / 3, 0, (dt**2) / 2],
-        [(dt**3) / 6, 0, (dt**2) / 2, 0, dt, 0],
-        [0, (dt**3) / 6, 0, (dt**2) / 2, 0, dt],
+        [(dt**5 / 20), (dt**4 / 8), (dt**3 / 6)],
+        [(dt**4 / 8), (dt**3 / 3), (dt**2 / 2)],
+        [(dt**3 / 6), (dt**2 / 2), dt],
     ]
 )
+Q_generator = lambda dt: np.block(
+    [[Q1D_generator(dt), np.zeros((3, 3))], [np.zeros((3, 3)), Q1D_generator(dt)]]
+)
 
-R = 1.0 * np.array([[var, 0], [0, var]])
+Q = Q_generator(dt)
 
-x0 = np.array([[x_initial], [y_initial], [1], [1], [1], [1]])
+x0 = np.array([[1], [0], [0], [1], [0], [0]])
 
-P0 = 1.0 * np.eye(6)
+# Starting error covariance (should be on the higher side to quickly settle in towards the correct values. i.e high uncertainty to start with :))
+P0 = 5 * np.eye(6)
 
-# # Run with no noise
-# range_sigma = 0
-# azimuth_sigma = np.deg2rad(0)
-# var_r = range_sigma**2
-# var_phi = azimuth_sigma**2
-# R = 1 * np.array([[var_r, 0], [0, var_phi]])
-# measurements, trueTrack = simulateRandomAccelTrackPolar(
-#     v_x=1,
-#     v_y=1,
-#     x0=x_initial,
-#     y0=y_initial,
-#     num_datapoints=num_datapoints,
-#     dt=dt,
-#     sigma_r=range_sigma,
-#     sigma_phi=azimuth_sigma,
-# )
-
-# animate_track(trueTrack, dt=dt)
-# RunJointKalman(
-#     f, h_polar, F, H_polar, Q, R, x0, P0, 0.01, 2, 0, measurements, trueTrack
-# )
-
-# Another with low sigma
-range_sigma = 1
+# Define variances in measurement dimensions.
+range_sigma = 3
 azimuth_sigma = np.deg2rad(3)
+
+# convert to variance.
 var_r = range_sigma**2
 var_phi = azimuth_sigma**2
-R = 1.0 * np.array([[var_phi, 0], [0, var_r]])
 
-measurements, trueTrack = simulateRandomAccelTrackPolar(
-    v_x=1,
-    v_y=1,
-    x0=x_initial,
-    y0=y_initial,
-    num_datapoints=num_datapoints,
+# Initialize measurement error matrix.
+R = 1 * np.array([[var_phi, 0], [0, var_r]])
+
+# Define measurement_model
+# measurement_model = CartesianToBearingRange(ndim_state=4, mapping=(0, 2), noise_covar=R)
+measurement_model = CartesianToBearingRange(ndim_state=6, mapping=(0, 3), noise_covar=R)
+
+# start the clock for easy timestamp management
+start_time = datetime.now()
+
+# Get the detections from Abdullahs group
+detections = ReadDetections(
+    filepath="Data/flight2/flight2-sidetoside-tiny_tinyrad_master_1_rd_guided_mvdr_range_angle_velocity_detections_ndoppler250.csv",
+    measurement_model=measurement_model,
     dt=dt,
-    sigma_r=range_sigma,
-    sigma_phi=azimuth_sigma,
-)
-# detections = RandomAccelHoverTrackPolar_stonesoup(
-#     v_x=1,
-#     v_y=1,
-#     x0=x_initial,
-#     y0=y_initial,
-#     num_datapoints=num_datapoints,
-#     dt=dt,
-#     sigma_r=range_sigma,
-#     sigma_phi=azimuth_sigma,
-# )
-
-# tuning
-Q_UCMKF = 0.5651266374815751 * Q
-P0_UCMKF = 0.5326018875626138 * P0
-Q_EKF = 4.759747414526448 * Q
-R_EKF = 4.15905619206011 * R
-P0_EKF = 0.8539349359398827 * P0
-Q_UKF = 0.005917359871277248 * Q
-R_UKF = 0.01368247610843965 * R
-P0_UKF = 0.019883725415176096 * P0
-alpha = 0.5615697188642643
-
-# animate_track(trueTrack, dt=dt)
-RunJointKalman(
-    f_matrix,
-    f,
-    h_cartesian_matrix,
-    h_polar,
-    F,
-    H_polar,
-    Q_UCMKF,
-    Q_EKF,
-    Q_UKF,
-    R_EKF,
-    R_UKF,
-    range_sigma,
-    azimuth_sigma,
-    x0,
-    P0_UCMKF,
-    P0_EKF,
-    P0_UKF,
-    2.0,
-    2,
-    0,
-    measurements,
-    trueTrack,
-)
-# TuneEKF(f, h_polar, F, H_polar, x0, var_r, var_phi, dt, 10)
-# TuneUKF(f, h_polar, x0, var_r, var_phi, dt, 2, 0, 10)
-# optimize_UCMKF(f_matrix, h_cartesian_matrix, x0, range_sigma, azimuth_sigma, dt, 1000)
-# optimize_EKF(f, h_polar, F, H_polar, x0, var_r, var_phi, dt, 1000)
-# optimize_UKF(f, h_polar, x0, 2, 0, var_r, var_phi, dt, 1000)
-
-BenchmarkJoint(
-    f_matrix,
-    f,
-    F,
-    h_cartesian_matrix,
-    h_polar,
-    H_polar,
-    Q_UCMKF,
-    Q_EKF,
-    Q_UKF,
-    R_EKF,
-    R_UKF,
-    x0,
-    P0_UCMKF,
-    P0_EKF,
-    P0_UKF,
-    range_sigma,
-    azimuth_sigma,
-    dt,
-    alpha,
-    2,
-    0,
-    1000,
+    start_time=start_time,
 )
 
-# # Another with high sigma
-# range_sigma = 10
-# azimuth_sigma = np.deg2rad(5)
-# var_r = range_sigma**2
-# var_phi = azimuth_sigma**2
-# R = 1 * np.array([[var_r, 0], [0, var_phi]])
-# measurements, trueTrack = simulateRandomAccelTrackPolar(
-#     v_x=1,
-#     v_y=1,
-#     x0=x_initial,
-#     y0=y_initial,
-#     num_datapoints=num_datapoints,
-#     dt=dt,
-#     sigma_r=range_sigma,
-#     sigma_phi=azimuth_sigma,
+
+# ___Initialize the filter____
+# ekf = ExtendedKalmanFilter(f=f, h=h, F=F, H=H, Q=Q, R=R, x0=x0, P0=P0)
+ukf = UnscentedKalmanFilter(f=f, h=h, Q=Q, R=R, x0=x0, P0=P0, alpha=1, beta=2, kappa=0)
+
+# predictor = EKFPredictor(ekf=ekf)
+# updater = EKFUpdater(ekf=ekf)
+predictor = UKFPredictor(ukf=ukf)
+updater = UKFUpdater(ukf=ukf)
+
+# prior = GaussianState(
+#     state_vector=x0,
+#     covar=ekf.P,
+#     timestamp=start_time,
 # )
 
-# animate_track(trueTrack, dt=dt)
-# RunJointKalman(
-#     f_matrix,
-#     f,
-#     h_cartesian_matrix,
-#     h_polar,
-#     F,
-#     H_polar,
-#     Q_UCMKF,
-#     Q_EKF,
-#     Q_UKF,
-#     R_EKF,
-#     R_UKF,
-#     range_sigma,
-#     azimuth_sigma,
-#     x0,
-#     P0_UCMKF,
-#     P0_EKF,
-#     P0_UKF,
-#     2.0,
-#     2,
-#     0,
-#     measurements,
-#     trueTrack,
-# )
-# TuneEKF(f, h_polar, F, H_polar, x0, var_r, var_phi, measurements, trueTrack)
-# TuneUKF(f, h_polar, x0, var_r, var_phi, 2, 0, measurements, trueTrack)
+prior = GaussianState(
+    state_vector=x0,
+    covar=ukf.P,
+    timestamp=start_time,
+)
+
+
+hypothesiser = DistanceHypothesiser(
+    predictor, updater, measure=Mahalanobis(), missed_distance=2
+)
+
+data_associator = GlobalNearestNeighbour(hypothesiser)
+
+deleter = CovarianceBasedDeleter(covar_trace_thresh=25.0)
+
+initiator = MultiMeasurementInitiator(
+    prior_state=prior,
+    deleter=deleter,
+    data_associator=data_associator,
+    updater=updater,
+    min_points=5,
+)
+# ________________________
+
+
+tracks, all_tracks = set(), set()
+timesteps = []
+previous_timestamp = None
+
+for n, measurements in enumerate(detections):
+
+    timestamp = start_time + timedelta(seconds=dt * n)
+    timesteps.append(timestamp)
+
+    hypotheses = data_associator.associate(tracks, measurements, timestamp)
+    associated_measurements = set()
+
+    for track in tracks:
+        hypothesis = hypotheses[track]
+
+        if hypothesis.measurement:
+            post = updater.update(hypothesis)
+            track.append(post)
+
+            associated_measurements.add(hypothesis.measurement)
+
+        else:
+            track.append(hypothesis.prediction)
+
+    tracks -= deleter.delete_tracks(tracks)
+    tracks |= initiator.initiate(measurements - associated_measurements, timestamp)
+    all_tracks |= tracks
+
+
+# Plotting
+plotter = Plotter()
+# plotter.plot_ground_truths(ground_truths, [0, 3])   # indices of x,y in state vector
+plotter.plot_tracks(all_tracks, [0, 3])
+plotter.plot_measurements(
+    [det for det_set in detections for det in det_set],
+    [0, 3],
+    measurement_model=measurement_model,
+)
+
+ax = plt.gca()
+x_min, x_max = ax.get_xlim()
+y_min, y_max = ax.get_ylim()
+data_min = min(x_min, y_min)
+data_max = max(x_max, y_max)
+# ax.set_xlim(data_min, data_max)
+# ax.set_ylim(data_min, data_max)
+ax.set_xlim(-10, 50)
+ax.set_ylim(-30, 30)
+ax.set_aspect("equal", adjustable="box")
+
+plt.grid()
+plotter.fig.show()
+plt.show()
+print(len(timesteps))
+from stonesoup.plotter import AnimatedPlotterly
+
+print("test")
+plotter = AnimatedPlotterly(timesteps, tail_length=1)
+print("test")
+plotter.fig.update_layout(width=700, height=700)
+plotter.plot_tracks(all_tracks, [0, 3], uncertainty=True)
+# plotter.plot_ground_truths(ground_truths, [0, 3])
+plotter.plot_measurements(detections, [0, 3])
+print("test")
+plotter.fig.show()
---
+++ main_old.py
@@ -0,0 +1,269 @@
+# External Imports
+import numpy as np
+
+# Internal Packages
+from Track_Simulation import (
+    simulateLinearTrack,
+    simulateLinearTrackPolar,
+    simulateRandomAccelTrackPolar,
+    simulateRandomAccelHoverTrackPolar,
+)
+from Tracking_Routines import (
+    RunSimpleKalman,
+    RunConvertedKalman,
+    RunExtendedKalman,
+    RunUnscentedKalman,
+    RunJointKalman,
+)
+from Utils import animate_track
+from Utils.KalmanBenchmarker import BenchmarkEKF, BenchmarkJoint, BenchmarkUKF
+from Utils.KalmanTuner import (
+    TuneEKF,
+    TuneUKF,
+    optimize_UCMKF,
+    optimize_EKF,
+    optimize_UKF,
+)
+
+# EXTENDED KALMAN FILTER
+# Initialize values
+dt = 0.4
+x_initial = 5
+y_initial = 5
+measurement_sigma = 0  # standard deviation of the measurement
+var = measurement_sigma**2
+
+num_datapoints = 60
+
+# Initialize the functions and matrices for the Kalman filter.
+f_matrix = np.array(
+    [
+        [1, 0, dt, 0, 0.5 * dt**2, 0],
+        [0, 1, 0, dt, 0, 0.5 * dt**2],
+        [0, 0, 1, 0, dt, 0],
+        [0, 0, 0, 1, 0, dt],
+        [0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 0, 1],
+    ]
+)
+f = lambda x: np.dot(
+    f_matrix,
+    x,
+)
+F = lambda x: np.array(
+    [
+        [1, 0, dt, 0, 0.5 * dt**2, 0],
+        [0, 1, 0, dt, 0, 0.5 * dt**2],
+        [0, 0, 1, 0, dt, 0],
+        [0, 0, 0, 1, 0, dt],
+        [0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 0, 1],
+    ]
+)
+
+h_cartesian = lambda x: np.array([x[0], x[1]])
+h_cartesian_matrix = np.array([[1, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0]])
+H_cartesian = lambda x: h_cartesian_matrix
+h_polar = lambda x: np.array(
+    [[np.arctan2(x[1], x[0])], [np.sqrt(x[0] ** 2 + x[1] ** 2)]]
+)  # conversion to polar
+H_polar = lambda x: np.array(
+    [
+        [
+            (-1 * x[1]) / (x[0] ** 2 + x[1] ** 2),
+            x[0] / (x[0] ** 2 + x[1] ** 2),
+            0,
+            0,
+            0,
+            0,
+        ],  # derivative of arctan2
+        [
+            x[0] / np.sqrt(x[0] ** 2 + x[1] ** 2),
+            x[1] / np.sqrt(x[0] ** 2 + x[1] ** 2),
+            0,
+            0,
+            0,
+            0,
+        ],
+    ]
+)
+
+# TODO: Find appropriate covariance matrices.
+Q = 1.0 * np.array(
+    [
+        [(dt**5) / 20, 0, (dt**4) / 8, 0, (dt**3) / 6, 0],
+        [0, (dt**5) / 20, 0, (dt**4) / 8, 0, (dt**3) / 6],
+        [(dt**4) / 8, 0, (dt**3) / 3, 0, (dt**2) / 2, 0],
+        [0, (dt**4) / 8, 0, (dt**3) / 3, 0, (dt**2) / 2],
+        [(dt**3) / 6, 0, (dt**2) / 2, 0, dt, 0],
+        [0, (dt**3) / 6, 0, (dt**2) / 2, 0, dt],
+    ]
+)
+
+R = 1.0 * np.array([[var, 0], [0, var]])
+
+x0 = np.array([[x_initial], [y_initial], [1], [1], [1], [1]])
+
+P0 = 1.0 * np.eye(6)
+
+# # Run with no noise
+# range_sigma = 0
+# azimuth_sigma = np.deg2rad(0)
+# var_r = range_sigma**2
+# var_phi = azimuth_sigma**2
+# R = 1 * np.array([[var_r, 0], [0, var_phi]])
+# measurements, trueTrack = simulateRandomAccelTrackPolar(
+#     v_x=1,
+#     v_y=1,
+#     x0=x_initial,
+#     y0=y_initial,
+#     num_datapoints=num_datapoints,
+#     dt=dt,
+#     sigma_r=range_sigma,
+#     sigma_phi=azimuth_sigma,
+# )
+
+# animate_track(trueTrack, dt=dt)
+# RunJointKalman(
+#     f, h_polar, F, H_polar, Q, R, x0, P0, 0.01, 2, 0, measurements, trueTrack
+# )
+
+# Another with low sigma
+range_sigma = 1
+azimuth_sigma = np.deg2rad(3)
+var_r = range_sigma**2
+var_phi = azimuth_sigma**2
+R = 1.0 * np.array([[var_phi, 0], [0, var_r]])
+
+measurements, trueTrack = simulateRandomAccelTrackPolar(
+    v_x=1,
+    v_y=1,
+    x0=x_initial,
+    y0=y_initial,
+    num_datapoints=num_datapoints,
+    dt=dt,
+    sigma_r=range_sigma,
+    sigma_phi=azimuth_sigma,
+)
+# detections = RandomAccelHoverTrackPolar_stonesoup(
+#     v_x=1,
+#     v_y=1,
+#     x0=x_initial,
+#     y0=y_initial,
+#     num_datapoints=num_datapoints,
+#     dt=dt,
+#     sigma_r=range_sigma,
+#     sigma_phi=azimuth_sigma,
+# )
+
+# tuning
+Q_UCMKF = 0.5651266374815751 * Q
+P0_UCMKF = 0.5326018875626138 * P0
+Q_EKF = 4.759747414526448 * Q
+R_EKF = 4.15905619206011 * R
+P0_EKF = 0.8539349359398827 * P0
+Q_UKF = 0.005917359871277248 * Q
+R_UKF = 0.01368247610843965 * R
+P0_UKF = 0.019883725415176096 * P0
+alpha = 0.5615697188642643
+
+# animate_track(trueTrack, dt=dt)
+RunJointKalman(
+    f_matrix,
+    f,
+    h_cartesian_matrix,
+    h_polar,
+    F,
+    H_polar,
+    Q_UCMKF,
+    Q_EKF,
+    Q_UKF,
+    R_EKF,
+    R_UKF,
+    range_sigma,
+    azimuth_sigma,
+    x0,
+    P0_UCMKF,
+    P0_EKF,
+    P0_UKF,
+    2.0,
+    2,
+    0,
+    measurements,
+    trueTrack,
+)
+# TuneEKF(f, h_polar, F, H_polar, x0, var_r, var_phi, dt, 10)
+# TuneUKF(f, h_polar, x0, var_r, var_phi, dt, 2, 0, 10)
+# optimize_UCMKF(f_matrix, h_cartesian_matrix, x0, range_sigma, azimuth_sigma, dt, 1000)
+# optimize_EKF(f, h_polar, F, H_polar, x0, var_r, var_phi, dt, 1000)
+# optimize_UKF(f, h_polar, x0, 2, 0, var_r, var_phi, dt, 1000)
+
+BenchmarkJoint(
+    f_matrix,
+    f,
+    F,
+    h_cartesian_matrix,
+    h_polar,
+    H_polar,
+    Q_UCMKF,
+    Q_EKF,
+    Q_UKF,
+    R_EKF,
+    R_UKF,
+    x0,
+    P0_UCMKF,
+    P0_EKF,
+    P0_UKF,
+    range_sigma,
+    azimuth_sigma,
+    dt,
+    alpha,
+    2,
+    0,
+    1000,
+)
+
+# # Another with high sigma
+# range_sigma = 10
+# azimuth_sigma = np.deg2rad(5)
+# var_r = range_sigma**2
+# var_phi = azimuth_sigma**2
+# R = 1 * np.array([[var_r, 0], [0, var_phi]])
+# measurements, trueTrack = simulateRandomAccelTrackPolar(
+#     v_x=1,
+#     v_y=1,
+#     x0=x_initial,
+#     y0=y_initial,
+#     num_datapoints=num_datapoints,
+#     dt=dt,
+#     sigma_r=range_sigma,
+#     sigma_phi=azimuth_sigma,
+# )
+
+# animate_track(trueTrack, dt=dt)
+# RunJointKalman(
+#     f_matrix,
+#     f,
+#     h_cartesian_matrix,
+#     h_polar,
+#     F,
+#     H_polar,
+#     Q_UCMKF,
+#     Q_EKF,
+#     Q_UKF,
+#     R_EKF,
+#     R_UKF,
+#     range_sigma,
+#     azimuth_sigma,
+#     x0,
+#     P0_UCMKF,
+#     P0_EKF,
+#     P0_UKF,
+#     2.0,
+#     2,
+#     0,
+#     measurements,
+#     trueTrack,
+# )
+# TuneEKF(f, h_polar, F, H_polar, x0, var_r, var_phi, measurements, trueTrack)
+# TuneUKF(f, h_polar, x0, var_r, var_phi, 2, 0, measurements, trueTrack)
